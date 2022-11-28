1788009dbd26ec57e970afc1948f403d647601e1
zling <zling@mie.utoronto.ca>
2022-11-28 02:51:58 -0500

    Update bender_cut.py

--- scripts/bender_cut.py
+++ scripts/bender_cut.py
@@ -58,86 +58,100 @@ def clustering_scenarios(problem, type, n_cluster, multi = True):
         return q_dict, W_dict, h_dict, T_dict, n_label, max_range
 
 
-
-def dropout_multicut(problem, type, n_cluster):
+def dropout_cut(problem, type, n_cluster):
     MP = gp.Model("MP")
     MP.Params.outputFlag = 0
 
     q_list, W_list, h_list, T_list, k = clustering_scenarios(problem, type, n_cluster)
 
-    x = MP.addMVar((problem.s1_n_var,), name = "x")
-    eta = MP.addMVar((k,), name = "eta", ub = problem.eta_bounds[1], lb = problem.eta_bounds[0])
-
+    x = MP.addMVar((problem.s1_n_var,), name="x")
+    eta = MP.addMVar((k,), name="eta", ub=problem.eta_bounds[1], lb=problem.eta_bounds[0])
     if problem.s1_direction == GRB.MAXIMIZE:
         MP.modelSense = GRB.MAXIMIZE
     else:
         MP.modelSense = GRB.MINIMIZE
 
     MP.setObjective(
-        problem.c @ x + np.array([1/k] * k) @ eta 
+        problem.c @ x + np.array([1 / k] * k) @ eta
     )
 
     c1 = MP.addConstr(
         problem.A @ x <= problem.b
     )
-
-
     cut_found = True
     n_iters = 0
-    try: 
-        while cut_found:
-            cut_found = False
-            q_vals = []
-            if n_iters < 200:
-                n_iters = n_iters + 1
-                print(f"On Iteration: {n_iters}")
-            else:
-                break
-            MP.update()
-            MP.optimize()
-            
-            x_i = x.x
-            eta_i = eta.x
-            LB = problem.c @ x_i
-
-            for s in range(k):
-                SP = gp.Model("SP")
-                SP.Params.outputFlag = 0  # turn off output
-                SP.Params.method = 1      # dual simplex
-                y = SP.addMVar((problem.s2_n_var,), name = "y", obj = q_list[s])
-                if problem.s2_direction == GRB.MAXIMIZE:
-                    SP.modelSense = GRB.MAXIMIZE
-                else:
-                    SP.modelSense = GRB.MINIMIZE
-                res = SP.addConstr(
-                    W_list[s] @ y <= h_list[s] - (T_list[s] @ x_i)
-                )
-                SP.optimize()
-                Q = SP.ObjVal
-                q_vals.append(Q)
-                pi = res.Pi
-                BestLB = 0
-                LB = LB + Q / k
-                if np.abs(Q - eta_i[s]) > 0.00001:
-                    if Q < eta_i[s]:
-                        cut_found = True
-                        p1 = pi @ h_list[s]
-                        p2 = pi @ T_list[s]
-                        if problem.s2_direction == GRB.MAXIMIZE:
-                            MP.addConstr(
-                                eta[s] <= p1 - gp.quicksum(p2[a] * x[a] for a in range(problem.s1_n_var))
-                            )
-                        else:
-                            MP.addConstr(
-                                eta[s] >= p1 - gp.quicksum(p2[a] * x[a] for a in range(problem.s1_n_var))
-                            )
-                if(LB > BestLB):
-                    BestLB = LB        
-            print(f"Iteration {n_iters}: LB = {BestLB}. UB = {MP.ObjVal}")
-    except:
-        print(f"Errored out on iteration: {n_iters} scenario {s}")
-
-
+    n_cuts = 0
+    MP_solve_time = 0
+    BL_solve_time = 0
+    status = "optimal"
+    highest_LB = 0
+    UB = np.abs(problem.eta_bounds[0]) * 10000
+    t1 = time.time()
+    while cut_found:
+        gc.collect()
+        curr_time = time.time()
+        if curr_time - t1 >= 300:
+            status = "timelimit"
+            break
+        n_iters = n_iters + 1
+        cut_found = False
+        MP.update()
+        t_mp_1 = time.time()
+        MP.optimize()
+        t_mp_2 = time.time()
+        MP_solve_time = MP_solve_time + t_mp_2 - t_mp_1
+        UB = MP.ObjVal
+        x_i = x.x
+        LB = problem.c @ x_i
+        eta_i = eta.x
+        t_bl_1 = time.time()
+        for s in range(k):
+            SP = gp.Model("SP")
+            SP.Params.outputFlag = 0  # turn off output
+            SP.Params.method = 1  # dual simplex
+            y = SP.addMVar((problem.s2_n_var,), name="y", obj=problem.q_list[s])
+            SP.modelSense = problem.s2_direction
+            res = SP.addConstr(
+                W_list[s] @ y <= h_list[s] - (T_list[s] @ x_i)
+            )
+            SP.optimize()
+            Q = SP.ObjVal
+            pi = res.Pi
+            LB = LB + Q / k
+            if np.abs(Q - eta_i[s]) > 0.00001:
+                if Q < eta_i[s]:
+                    cut_found = True
+                    p1 = pi @ h_list[s]
+                    p2 = pi @ T_list[s]
+                    MP.addConstr(
+                        eta[s] <= p1 - gp.quicksum(p2[a] * x[a] for a in range(problem.s1_n_var))
+                    )
+                    n_cuts = n_cuts + 1
+        t_bl_2 = time.time()
+        BL_solve_time = BL_solve_time + t_bl_2 - t_bl_1
+        if LB > highest_LB:
+            highest_LB = LB
+    t2 = time.time()
+    elapsed_time = t2 - t1
+    results = {
+        "method": "dropout-cut",
+        "obj_val": MP.ObjVal,
+        "n_cuts": n_cuts,
+        "n_iterations": n_iters,
+        "avg_mp_solve": MP_solve_time / n_iters,
+        "avg_benders_loop_solve": BL_solve_time / n_iters,
+        "status": status,
+        "primal_gap": UB - highest_LB,
+        "primal_gap_perc": (UB - highest_LB) / UB,
+        "runtime": elapsed_time,
+        "n1": problem.s1_n_var,
+        "n2": problem.s2_n_var,
+        "m1": problem.s1_n_constr,
+        "m2": problem.s2_n_constr,
+        "k": problem.k,
+        "distribution": problem.distribution
+    }
+    return results
 
 def hybrid(problem, type, n_cluster):
     MP = gp.Model("MP")
